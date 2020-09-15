d2bac872d1e889d84ddd3a3d098947afeeac1e8b
Meredith McIntosh <meredith@mcintosh.io>
2020-09-15 19:02:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,14 @@
-# def using_push(array,string)
-#   array.push(string)
-# end
 
-# def using_unshift(array, string)
-#   array.unshift(string)
-# end
-
-# def using_pop(array)
-#   array.pop
-# end
 
 def pop_with_args(array)
   array.pop(2)
 end
 
-# def using_shift(array)
-#   array.shift
-# end
+def using_shift(array)
+  array.shift
+end
 
 def shift_with_args(array)
-  array.shift
+  array.shift(2)
 end
 
